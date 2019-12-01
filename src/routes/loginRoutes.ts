308cfba2ac6b3d9b53289fd20942c97547e18c4f
CheezItMan <chris@adadevelopersacademy.org>
2019-12-01 13:09:26 -0800

    added routes for logging in and out, as well as a protected section with middleware

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,15 +1,43 @@
-import { Router } from 'express';
+import { Router, NextFunction } from 'express';
 import express, { Request, Response } from 'express';
 import { request } from 'http';
+import { readSync } from 'fs';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined },
+};
+
+const requireAuth = function(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    return next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
+}
 
 export const router = Router();
+const UNAUTHORIZED = 401;
+const UNPROCESSABLE_ENTITY = 422;
 
 router.get('/', (req: Request, res: Response) => {
-  res.send(`
-  <div>
-    <h1>Heya</h1>
-  </div>
-  `);
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <h2>You are logged in!</h2>
+      <p><a href="/logout">Logout</a></p>
+    </div>
+    `);
+
+  } else {
+    res.send(`
+    <div>
+      <h1>Heya</h1>
+      <p>Please login with <a href="/login">This link</a></p>
+    </div>
+    `);
+  }
 });
 
 router.get('/login', (req: Request, res: Response) => {
@@ -35,14 +63,45 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
-  const { email, password }: { } = req.body;
-  console.log(req);
-  res.send(`
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email && password &&
+    email === 'bob@bob.com' && password === 'password') {
+    req.session = {
+      loggedIn: true,
+    };
+
+    res.redirect('/');
+  } else if (email && password) {
+    res.status(UNAUTHORIZED);
+    res.send(`
     <div>
-      <h1>Thank you</h1>
-      <p>Email: ${email}</p>
-      <p>Password: ${password}</p>
+      <h2>Unauthorized</h2>
     </div>
+    `);
+  }
+  else {
+    res.status(UNPROCESSABLE_ENTITY);
+    res.send(`
+    <div>
+      <h2>You need to submit both an email & body</h2>
+    </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.loggedIn = false;
+  }
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send(`
+  <div>
+    <h2>Welcome to protected stuff</h2>
+  </div>
   `);
 });
