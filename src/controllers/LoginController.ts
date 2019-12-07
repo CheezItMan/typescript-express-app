c3e2d25696c91d7dae6f6c5b0427a7d31b310784
CheezItMan <chris@adadevelopersacademy.org>
2019-12-07 10:41:35 -0800

    handle get /auth/login

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,31 +1,22 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { get, controller } from './decorators';
 
-import { get } from './decorators/routes';
-
-@controller('/')
-export class LoginController {
-
+@controller('/auth')
+class LoginController {
   @get('/login')
   getLogin(req: Request, res: Response): void {
     res.send(`
-    <div>
-      <h1>Login form</h1>
-      <div>
       <form method="POST">
         <div>
-          <label for="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <label>Email</label>
+          <input name="email" />
         </div>
         <div>
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <label>Password</label>
+          <input name="password" type="password" />
         </div>
-        <div>
-          <input type="submit" value="login" id="submit-btn" name="submit-btn" class="btn submit primary" />
-        </div>
+        <button>Submit</button>
       </form>
-      </div>
-    </div>
     `);
-  });
+  }
 }
