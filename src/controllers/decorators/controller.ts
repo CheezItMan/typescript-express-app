5e0de994922dc8e33015fdcd0abbbb44bcc7d09e
CheezItMan <chris@adadevelopersacademy.org>
2019-12-07 10:41:10 -0800

    decorators

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,11 +1,23 @@
 import 'reflect-metadata';
+import { AppRouter } from '../../AppRouter';
+import { Methods } from './Methods';
 
-const controller = (routePrefix: string) => {
+const controller = function(routePrefix: string) {
   return function (target: Function) {
+    const router = AppRouter.getInstance();
+
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-
       const path = Reflect.getMetadata('path', target.prototype, key);
+      const method: Methods = Reflect.getMetadata(
+        'method',
+        target.prototype,
+        key
+      );
+
+      if (path) {
+        router[method](`${routePrefix}${path}`, routeHandler);
+      }
     }
   }
 }
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,9 +1,17 @@
 import 'reflect-metadata';
+import { Methods } from './Methods';
 
-const get = (path: string) => {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    }
   }
 }
 
-export {get};
+export const get = routeBinder(Methods.get);
+export const put = routeBinder(Methods.put);
+export const patch = routeBinder(Methods.patch);
+export const del = routeBinder(Methods.del);
+export const post = routeBinder(Methods.post);
