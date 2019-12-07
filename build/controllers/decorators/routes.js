6e90a74ef4efc200d21e5c1d403c0901696256f3
CheezItMan <chris@adadevelopersacademy.org>
2019-12-07 10:42:22 -0800

    build

---
+++ build/controllers/decorators/routes.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+var Methods_1 = require("./Methods");
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
+    };
+}
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.patch = routeBinder(Methods_1.Methods.patch);
+exports.del = routeBinder(Methods_1.Methods.del);
+exports.post = routeBinder(Methods_1.Methods.post);
+//# sourceMappingURL=routes.js.map
