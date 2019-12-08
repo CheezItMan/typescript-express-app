1a475885db152c898695a33a7ba6239eaa76c8ab
CheezItMan <chris@adadevelopersacademy.org>
2019-12-08 12:34:50 -0800

    added middleware decorator

---
+++ src/controllers/decorators/use.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import { RequestHandler } from 'express';
+import { MetaDataKeys } from './MetadataKeys';
+
+export function use(middleware: RequestHandler) {
+
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    const middlewares = Reflect.getMetadata(
+      MetaDataKeys.Middleware,
+      target,
+      key
+    ) || [];
+
+    Reflect.defineMetadata(
+      MetaDataKeys.Middleware,
+      [...middlewares, middleware],
+      target,
+      key
+    );
+  }
+
+}
+
